95e18b18fd60ee7825c87e87ad99f03fd44e3f02
ASHFAK <ashfak.mulla778@gmail.com>
2025-01-27 19:59:30 +0530

    THIS IS MY FIRST COMMIT

---
+++ as.sh
@@ -0,0 +1,7 @@
+echo "THIS IS ECHO"
+echo "A"
+read a
+echo "B"
+read b
+c=`expr $a + $b`
+echo "ADDITION =" $c
---
+++ ash.py
@@ -0,0 +1,5 @@
+print("ENTER THE ADDITION VALUES")
+a=int(input("ENTER THE VALUE OF A ="))
+b=int(input("ENTER THE VALUE OF B ="))
+c=a+b
+print("THE ADDITION OF TWO NUMBNER IS = ",c)
